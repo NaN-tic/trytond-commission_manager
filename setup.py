c063282cde85dc14e77c8f66b32be2c0b25a2ab6
Raimon Esteve <raimonesteve@gmail.com>
2025-06-18 13:47:21 +0200

    FIX Removing deprecated args from setup

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ],
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst']),
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
